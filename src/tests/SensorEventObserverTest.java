a946482e0170acb7d34d84c1c3646d8d122a960b
IskanderNakipov <iskander.sych@gmail.com>
2017-12-17 14:16:04 +0300

    SensorEventObserverTest + XML

---
+++ src/tests/SensorEventObserverTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import ru.sbt.mipt.oop.entities.SmartHome;
+import ru.sbt.mipt.oop.processors.*;
+import ru.sbt.mipt.oop.sensors.SensorEventObserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SensorEventObserverTest {
+    @Test
+    public void setHandlers() throws Exception {
+        SmartHome home = new SmartHome();
+        SensorEventObserver observer = new SensorEventObserver(home);
+        List<EventHandler> eventHandlers = new ArrayList<>();
+        eventHandlers.add(new LightEventProcessor());
+        eventHandlers.add(new DoorEventProcessor());
+        eventHandlers.add(new AutoEventProcessor());
+
+        for (EventHandler handler: eventHandlers) {
+            observer.addHandler(handler);
+        }
+
+        assertEquals(3, observer.processors.size());
+        for (EventHandler handler: eventHandlers){
+            assertTrue(observer.processors.contains(handler));
+        }
+    }
+
+}
